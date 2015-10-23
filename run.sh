c45b66f87f87831b23026ba3e0ef254e461fe1f5
ssosna <sergio.sosna@gmail.com>
2015-10-23 16:11:51 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ function pause(){
 
 wget -nc https://dl.bintray.com/mitchellh/packer/packer_0.8.6_linux_amd64.zip 
 unzip packer_0.8.6_linux_amd64.zip
-packer validate createami.json
+./packer validate createami.json
 #pause 'Press [Enter] key to continue...'
 
-packer build -var-file=pwd.json createami.json
+./packer build -var-file=pwd.json createami.json
