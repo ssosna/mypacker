fd83df1baf83f1a2c96c104a3e631712dc6387d3
ssosna <sergio.sosna@gmail.com>
2015-10-23 15:55:51 +0300

    run edit

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ function pause(){
    read -p "$*"
 }
 
+wget -qO- -Nr https://dl.bintray.com/mitchellh/packer/packer_0.8.6_linux_amd64.zip
+unzip packer_0.8.6_linux_amd64.zip
 packer validate createami.json
 #pause 'Press [Enter] key to continue...'
 
